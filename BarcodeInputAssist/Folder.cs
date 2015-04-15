becb1a2148b3de4d76e91f694ec83881fb778d5c
kingofcrabs <rex.xie@qq.com>
2015-04-15 18:47:40 +0800

    add

---
+++ BarcodeInputAssist/Folder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeInputAssist
+{
+    public class FolderHelper
+    {
+        static public string GetExeFolder()
+        {
+            string s = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return s + "\\";
+        }
+
+        static public string GetExeParentFolder()
+        {
+            string s = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            int index = s.LastIndexOf("\\");
+            return s.Substring(0, index) + "\\";
+        }
+
+        static public string GetConfigFolder()
+        {
+            return GetExeFolder() + "Config\\";
+        }
+    }
+}
